81642e9eaf7a67c27ce7e89612f19eff904b61b2
James ZOU <youzhibicheng@163.com>
2014-10-01 07:54:53 +0800

    git branch local

--- git_branch_local.sh
+++ git_branch_local.sh
@@ -11,8 +11,8 @@ git checkout branch
 git branch
 
 # based on git test branch, to do some change
-vim git_branch.txt
-git add git_branch.txt
+vim git_branch_local.txt
+git add git_branch_local.txt
 git commit -m 'test git branch on branch test'
 git status
 
@@ -27,27 +27,27 @@ git branch
 git branch test
 
 git checkout master
-vim git_branch.txt
+vim git_branch_local.txt
 # add "test git branch in branch mastear"
-git add git_branch.txt
+git add git_branch_local.txt
 git commit -m 'test git branch in branch master'
 git status
 
 git checkout test
-vim git_branch.txt
+vim git_branch_local.txt
 # add "test git branch in branch test"
-git add git_branch.txt
+git add git_branch_local.txt
 git commit -m 'test git branch in branch test'
 git status
 
 git checkout master
 git merge test
 # will give conflict error information, should add just by youself
-# Auto-merging git_branch.txt
-# CONFLICT (content): Merge conflict in git_branch.txt
+# Auto-merging git_branch_local.txt
+# CONFLICT (content): Merge conflict in git_branch_local.txt
 # Automatic merge failed; fix conflicts and then commit the result.
 git status
-# the git_branch.txt already merge and change, fix it and then do the following
+# the git_branch_local.txt already merge and change, fix it manually and then do the following
 git commit -a -m 'fix git branch merge conflict manually'
 git status
 git merge test

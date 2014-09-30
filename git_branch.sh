95d55704796c8f0bf1bdf50fb265a12f04615a26
James ZOU <youzhibicheng@163.com>
2014-10-01 07:41:58 +0800

    test git branch merge conflict

--- git_branch.sh
+++ git_branch.sh
@@ -12,4 +12,36 @@ git branch
 
 # based on git test branch, to do some change
 vim git_branch.txt
+git add git_branch.txt
+git commit -m 'test git branch on branch test'
+git status
+
+git checkout master
+git merge test
+git branch
+git branch -d test
+# to force delete
+git branch -D test
+git branch
+
+git branch test
+
+git checkout master
+vim git_branch.txt
+# add "test git branch in branch mastear"
+git add git_branch.txt
+git commit -m 'test git branch in branch master'
+git status
+
+git checkout test
+vim git_branch.txt
+# add "test git branch in branch test"
+git add git_branch.txt
+git commit -m 'test git branch in branch test'
+git status
+
+git checkout master
+git merge test
+# will give conflict error information, should add just by youself
+
 

36856355b38fb6415834ac5049cd3253101f1858
James ZOU <youzhibicheng@163.com>
2014-10-01 07:33:32 +0800

    test git branch in branch test

---
+++ git_branch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#  分支 branch
+
+# 操作本地分支
+git branch
+git branch test
+git branch
+
+git checkout branch
+git branch
+
+# based on git test branch, to do some change
+vim git_branch.txt
+
